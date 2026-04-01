52f391792d0081b03cef509d8f980ab58a9611c6
josefragala <joseffn2001@gmail.com>
2026-04-01 09:47:42 -0300

    EndPoint POST adicionado para criar lançamento

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -11,7 +11,15 @@ import org.springframework.http.ResponseEntity;
 import com.example.algamoney.api.repository.LancamentoRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestBody;
+import javax.validation.Valid;
+import org.springframework.context.ApplicationEventPublisher;
+import com.example.algamoney.api.event.RecursoCriadoEvent;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpServletResponse;
+
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -20,9 +28,19 @@ public class LancamentoResource {
     @Autowired
     private LancamentoRepository lancamentoRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     public List<Lancamento> listar() {
         return lancamentoRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+        Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
     }
 
     @GetMapping("/{codigo}")
